185321c11701f7d4dde1bd65c356113c6e12ca6a
mateuskuritza <mateuskuritza@gmail.com>
2022-02-11 10:32:25 -0300

    Preparando revisão de código

--- src/routes/signUpRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { signUp } from "../controllers/signUpController.js"
-import signUpValidateSchemaMiddleware from "../middlewares/signUpValidateSchemaMiddleware.js";
-
-const signUpRouter = Router()
-
-signUpRouter.post("/signup",signUpValidateSchemaMiddleware, signUp)
-
-export default signUpRouter;
